b7d69bb367b8a89b6b2e359413e9af75b8390bea
Christian Zunker <christian.zunker@gmail.com>
2012-04-09 11:54:12 +0200

    Updated libs and calling function to be compatible with selenium server 2.20.0

---
+++ src/com/example/tests/GoogleSeleniumWebdriverTestCase.java
@@ -0,0 +1,48 @@
+package com.example.tests;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class GoogleSeleniumWebdriverTestCase {
+	private WebDriver driver;
+	private String baseUrl;
+	private StringBuffer verificationErrors = new StringBuffer();
+	@Before
+	public void setUp() throws Exception {
+		driver = new FirefoxDriver();
+		baseUrl = "http://www.google.de/";
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	}
+
+	@Test
+	public void testGoogleSeleniumWebdriverTestCase() throws Exception {
+		driver.get(baseUrl + "/");
+		driver.findElement(By.id("gbqfq")).clear();
+		driver.findElement(By.id("gbqfq")).sendKeys("selenium hq");
+		// ERROR: Caught exception [ERROR: Unsupported command [isTextPresent]]
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		driver.quit();
+		String verificationErrorString = verificationErrors.toString();
+		if (!"".equals(verificationErrorString)) {
+			fail(verificationErrorString);
+		}
+	}
+
+	private boolean isElementPresent(By by) {
+		try {
+			driver.findElement(by);
+			return true;
+		} catch (NoSuchElementException e) {
+			return false;
+		}
+	}
+}
